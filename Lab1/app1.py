cb77692d6355973b11d9c55d8ab4b908a2de15ea
Khoaa Minh <khoa.2374802010241@vanlanguni.vn>
2025-05-29 19:34:26 +0700

    Handle POST form và thêm result.html

--- Lab1/app1.py
+++ Lab1/app1.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        
+        username   = request.form.get('username')
+        password   = request.form.get('password')
+        gender     = request.form.get('gender')
+        membership = request.form.get('membership')
+        favcolor   = request.form.get('favcolor')
+        correct    = 'correct' if request.form.get('correct') else 'not correct'
+
+        
+        return render_template('result.html',
+                               username=username,
+                               password=password,
+                               gender=gender,
+                               membership=membership,
+                               favcolor=favcolor,
+                               correct=correct)
+
+   
     return render_template('exercise3.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
